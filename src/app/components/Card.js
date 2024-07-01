3e9daf210005f37ad49e9fe2b23c491b624bf3a8
Erdemkhishig <erdmee1104@gmail.com>
2024-07-02 00:02:15 +0800

    ddd

---
+++ src/app/components/Card.js
@@ -0,0 +1,14 @@
+export const Card = () => {
+
+
+    return (
+        <div className=" border-2 border-gray-100 rounded-lg px-4 py-4 mb-8">
+        <div className="flex flex-col gap-8"><img src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?q=80&w=1176&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <p className="text-blue-500  px-2 w-28 h-8 border-2 border-gray-200 rounded-lg bg-gray-200">Technology</p>
+            <p className="font-bold">The Impact of Technology on the Workplace: How Technology is Changing</p>
+            <p className="text-gray-400">August 20, 2022</p>
+        </div>
+    </div>
+    
+
+    )}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import { Navbar } from "@/app/components/Navbar";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-white">
+      <Navbar />
+    
     </main>
   );
 }
