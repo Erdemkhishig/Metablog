1e1a8a015b332be9bb14b2adf40b75f48dc88ad3
Erdemkhishig <erdmee1104@gmail.com>
2024-07-03 00:40:09 +0800

    zx

--- src/app/components/LoadMore.js
+++ src/app/components/LoadMore.js
@@ -1,8 +1,8 @@
 export const Loadmore = () => {
 
     return (
-        <div className="w-full flex justify-center">
-            <div className="my-16 w-24 h-12 border-2 border-gray-400 rounded-lg flex justify-center items-center">
+        <div className="w-full flex justify-center ">
+            <div className="my-16 w-24 h-12 border-2 border-gray-400 rounded-lg flex justify-center items-center cursor-pointer ">
                 Load More
             </div>
         </div>
