66bd98044ddd4de7a6ef91e00ebefdb5cd821692
24LP4153 24LP4153 <24LP4153@G301-CC0195.local>
2024-07-19 12:35:42 +0800

    aaa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,8 @@ import { Search } from "./Search";
 import { SlArrowDownCircle } from "react-icons/sl";
 import { useState } from "react";
 import { Sidebar } from "./Sidebar";
+import { usePathname } from "next/navigation";
+import Home from "@/app/page";
 
 
 
@@ -18,6 +20,23 @@ export const Navbar = () => {
         unchecked(!checked)
     };
 
+    // const pathname = usePathname();
+    // const path = [
+    //     {
+    //         name: "Home",
+    //         path: "/",
+
+    //     },
+    //     {
+    //         name: "Blog",
+    //         path: "/blog",
+
+    //     }, {
+    //         name: "Contact",
+    //         path: "/contact",
+
+    //     }
+    // ]
 
     return (
 
@@ -26,6 +45,11 @@ export const Navbar = () => {
                 <Link href="/"><span className="pr-8"> <MetablogIcon /></span></Link>
                 <div className="hidden lg:flex text-[#696A75] items-center">
                     <div className="flex items-center gap-16 pl-8 pr-16">
+                        {/* {path.map((path, index) => {
+                            <Link key={index} href={path.path}>
+                                <div className="text-black "></div>
+                            </Link>
+                        })} */}
                         <Link href="/"><span>Home</span></Link>
                         <Link href="/blog"><span>Blog</span></Link>
                         <Link href="/contact"><span>Contact</span></Link>
@@ -42,7 +66,7 @@ export const Navbar = () => {
                 <button className="lg:hidden -mt-8" onClick={handleSidebar} ><DrawerIcon size={32} /></button>
 
             </div>
-            <div className={checked ? 'duration-500  translate-x-0 right-0 fixed top-0 w-9/12 h-max bg-white flex flex-col' : "translate-x-[100%] duration-500 right-0 fixed top-0 w-9/12 h-max  bg-white flex flex-col"}><Sidebar check={checked} setChecked={handleSidebar} /></div>
+            <div className={checked ? 'duration-500  translate-x-0 right-0 fixed top-0 w-full h-max bg-white flex flex-col' : "translate-x-[100%] duration-500 right-0 fixed top-0 w-9/12 h-max  bg-white flex flex-col"}><Sidebar check={checked} setChecked={handleSidebar} /></div>
 
 
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,11 +12,11 @@ export const Sidebar = ({ checked, setChecked }) => {
 
 
         <div className=" top-0 right-0 w-full h-max bg-white flex flex-col z-50">
-            <div className="text-2xl px-4 flex flex-row items-center justify-between border-b-2 border-gray-200 py-4 font-black">
+            <div className="text-2xl px-4 flex flex-row items-start justify-between border-b-2 border-gray-200 py-1 font-black">
                 <span><DrawerIcon size={32} /> </span>
-                <button onClick={setChecked}>X</button>
+                <button className="text-sm border-2 rounded-full border-black p-1 px-2" onClick={setChecked}>X</button>
             </div>
-            <div className="px-4 my-4 dark:text-white flex flex-row items-center gap-4 justify-between">
+            <div className="px-4 my-4 dark:text-white flex flex-row items-start gap-4 justify-between">
                 <Link href="/"><span>Home</span></Link>
                 <Link href="/blog"><span>Blog</span></Link>
                 <Link href="/contact"><span>Contact</span></Link>
