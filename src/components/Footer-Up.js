06d159a9c5386401a8f55f4b30bf00c54045b761
24LP4153 24LP4153 <24LP4153@G301-CC0195.local>
2024-07-22 12:26:28 +0800

    ddd

--- src/components/Footer-Up.js
+++ src/components/Footer-Up.js
@@ -3,6 +3,7 @@ import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 
 
@@ -10,6 +11,24 @@ import Link from "next/link";
 
 export const FooterUp = () => {
 
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const res = await fetch('http://localhost:3001/');
+
+                const data = await res.json();
+
+                console.log(data);
+                setData(data)
+            } catch (error) {
+                console.log(error)
+            }
+        };
+
+        getData();
+    }, [])
 
     return (
         <div className=" bg-gray-100 ">
@@ -22,10 +41,17 @@ export const FooterUp = () => {
                     <p className="mt-8 text-[#3B3C4A]">Phone : 880 123 456 789</p>
 
                 </div>
-                <div className="flex gap-12 py-8 text-[#696A75] lg:pt-4 lg:flex-col lg:gap-2">
+                <div className="flex flex-wrap gap-12 py-8 text-[#696A75] lg:pt-4 lg:flex-col lg:gap-2">
                     <Link href="/"><span>Home</span></Link>
                     <Link href="/blog"><span>Blog</span></Link>
                     <Link href="/contact"><span>Contact</span></Link>
+                    {/* {data.map((item) => (
+                        <div key={item.title}>
+                            <h1>{item.title}</h1>
+
+                        </div>
+
+                    ))} */}
                 </div>
                 <div className="flex gap-8 lg:pt-4">
                     <FaFacebook color="#6D6E76" />
